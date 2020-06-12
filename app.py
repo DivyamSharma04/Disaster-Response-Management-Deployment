c4480027b4b7c1a563a02b02028b15d6f05aeef0
Divyam Sharma <divyamsharma960@gmail.com>
2020-06-12 13:00:22 +0530

    second

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-import plotly
+
 import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
@@ -7,7 +7,7 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
